449f0b69e1deb3812f2b0e6195237ee5a80328a5
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-02-16 21:48:53 +0200

    LocationsCreatorFromJSON improvements

--- src/com/tiksem/pq/db/LocationsCreatorFromJSON.java
+++ src/com/tiksem/pq/db/LocationsCreatorFromJSON.java
@@ -7,9 +7,13 @@ import com.tiksem.pq.data.Country;
 import com.tiksem.pq.data.ProgressOperation;
 import com.utils.framework.CollectionUtils;
 import com.utils.framework.Predicate;
+import com.utils.framework.collections.map.TwoKeysHashMap;
 import com.utils.framework.google.translate.GoogleTranslate;
+import com.utils.framework.io.CsvIterator;
+import com.utils.framework.io.FileNonEmptyLineIterator;
 import com.utils.framework.io.IOUtilities;
 import com.utils.framework.parsers.json.JsonCollections;
+import com.utils.framework.strings.Strings;
 import com.vkapi.location.*;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
@@ -28,6 +32,7 @@ public class LocationsCreatorFromJSON {
     private String fileName;
     private List<Country> countries;
     private DatabaseAsyncTaskManager.Handler asyncTaskHandler;
+    private TwoKeysHashMap<String, String, String> cityTranslationMap = new TwoKeysHashMap<String, String, String>();
 
     public LocationsCreatorFromJSON(MysqlObjectMapper mapper, String fileName) {
         this.mapper = mapper;
@@ -68,19 +73,45 @@ public class LocationsCreatorFromJSON {
         return result;
     }
 
+    private String translateCity(String countryName, String cityName) {
+        return cityTranslationMap.remove(countryName, cityName);
+    }
+
+    private void initCityTranslationMap() throws FileNotFoundException {
+        CsvIterator enCsv = CsvIterator.fileIterator("locations_en.csv", "UTF-8");
+        CsvIterator ruCsv = CsvIterator.fileIterator("locations_ru.csv", "UTF-8");
+
+        //skip header
+        enCsv.next();
+        ruCsv.next();
+
+        while (enCsv.hasNext() && ruCsv.hasNext()) {
+            String[] en = enCsv.next();
+            String[] ru = ruCsv.next();
+            String countryName = en[5].replace("\"", "");
+            String enCityName = en[10].replace("\"", "");
+            String ruCityName = ru[10].replace("\"", "");
+            if (!Strings.isEmpty(ruCityName)) {
+                cityTranslationMap.put(countryName, enCityName, ruCityName);
+            }
+        }
+    }
+
     public void initLocations() throws IOException, JSONException {
         MysqlTablesCreator tablesCreator = new MysqlTablesCreator(mapper);
         tablesCreator.updateAndCreateTables(
                 Arrays.asList(City.class, Country.class), null, null);
         String source = IOUtilities.toString(new FileInputStream(fileName));
         JSONObject json = new JSONObject(source);
-        final Map<String, List<String>> map = JsonCollections.asStringListMap(json);
+        final Map<String, Set<String>> map = JsonCollections.toStringSetMap(json);
         List<String> enNames = new ArrayList<String>(map.keySet());
 
         final ProgressOperationManager progressOperationManager = new ProgressOperationManager(mapper);
         progressOperationManager.newOperation("Resolving Countries from vk", -1);
 
         final VkCountriesInfo vkCountriesInfo = getCountriesVkInfo();
+        initCityTranslationMap();
+
         countries = CollectionUtils.transform(enNames, new CollectionUtils.Transformer<String, Country>() {
             @Override
             public Country get(String enName) {
@@ -97,46 +128,45 @@ public class LocationsCreatorFromJSON {
 
         mapper.insertAll(countries);
 
-        boolean a = false;
-        if(a){
-            return;
-        }
-
         countries = mapper.executeSQLQuery("SELECT * FROM country", Country.class);
 
         progressOperationManager.newOperation("Resolving cities of countries...", countries.size());
 
-        List<City> cities = new ArrayList<City>();
+        final List<City> cities = new ArrayList<City>();
         for (final Country country : countries) {
             final int countryId = country.getId();
-            List<String> cityNames = map.get(country.getEnName());
+            final String countryName = country.getEnName();
+            Set<String> cityNames = map.get(country.getEnName());
             CollectionUtils.transformAndAdd(cityNames, cities, new CollectionUtils.Transformer<String, City>() {
                 @Override
                 public City get(String name) {
                     City city = new City();
                     city.setEnName(name);
+                    city.setId(cities.size() + 1);
                     city.setCountryId(countryId);
-
-                    try {
-                        Short vkCountryId = vkCountriesInfo.ides.get(country.getEnName());
-                        if (vkCountryId != null) {
-                            List<VkCity> vkCities = VkLocationApi.searchCities(name, vkCountryId,
-                                    Language.ru, 1);
-                            if(!vkCities.isEmpty()){
-                                String ruName = vkCities.get(0).title;
-                                city.setRuName(ruName);
-                            }
-                        }
-                    } catch (VkException e) {
-
-                    }
-
-                    progressOperationManager.setProgress(countries.indexOf(country));
+                    String ruName = translateCity(countryName, name);
+                    city.setRuName(ruName);
 
                     return city;
                 }
             });
+
+            Map<String, String> additionalCities = cityTranslationMap.getMap(countryName);
+            if (additionalCities != null) {
+                for(Map.Entry<String, String> entry : additionalCities.entrySet()){
+                    City city = new City();
+                    city.setEnName(entry.getKey());
+                    city.setId(cities.size() + 1);
+                    city.setCountryId(countryId);
+                    city.setRuName(entry.getValue());
+                    cities.add(city);
+                }
+            }
+
+            progressOperationManager.setProgress(countries.indexOf(country));
         }
+
+
 
         try {
             progressOperationManager.newOperation("Inserting cities...", cities.size());
@@ -156,10 +186,13 @@ public class LocationsCreatorFromJSON {
                     }
                 }
             });
+
             mapper.insertAll(cities, true);
             thread.interrupt();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            progressOperationManager.destroy();
         }
     }
 }
